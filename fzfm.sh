620fa9b3104b2d191478653fc0fa9f284a383c56
Er V P Singh <gmhpfc@gmail.com>
2024-03-17 20:55:06 +0530

    added m first bash script -file browser using zoxide and fzf

---
+++ fzfm.sh
@@ -0,0 +1,50 @@
+#! /usr/bin/env bash
+# building a terminal file manager in bash youtube
+# error catching otherwise script continue envn after failiar
+set -euo pipefail
+# error e exit on cmd fail  ,  u for undefined variable, o option name e.g pipefail
+fzfm() {
+	while true; do
+		selection="$(
+			lsd -a | fzf --reverse \
+				--height 95% \
+				--info right \
+				--prompt "Search: " \
+				--border "bold" \
+				--border-label "$(pwd)/" --prompt "Search: " \
+				--bind "left:pos(2)+accept" \
+				--bind "right:accept" \
+				--bind "shift-up:preview-up" \
+				--bind "shift-down:preview-down" \
+				--preview-window=right:65% \
+				--preview 'cd_pre="$(echo $(pwd)/$(echo {}))";
+                    echo "Folder: " $cd_pre;
+                    echo;
+                    lsd -a --color=always "${cd_pre}";
+                    txt_file="$(file {} | grep '[tT]ext' |wc -l)";
+                    if [[ "${txt_file}" -eq 1 ]]; then 
+                      # cat -t {} 2>/dev/null 
+                      bat --style=numbers --theme=ansi --color=always  {} 2>/dev/null
+                    else
+                      chafa -c full --color-space rgb --dither none -p on -w 1 --size=20x6 {} 2>/dev/null
+                      # chafa -c full --color-space rgb --dither none -p on -w 9 {} 2>/dev/null
+                    fi
+                   
+                    '
+		)"
+
+		if [[ -d "${selection}" ]]; then
+			>/dev/null cd "${selection}"
+		else
+			break
+		fi
+	done
+}
+clear
+fzfm
+# # fzf --reverse  --info right --prompt "Search: " --border "bold" --border-label "$(pwd)/" --prompt "Sea
+# rch: " --bind "left:pos(2)+accept" --bind "right:accept" --bind "shift-up:preview-up" --bind "shift-down
+# :preview-down"  --preview 'cd_pre="$(echo $(pwd)/#echo {})";
+#                     echo "Folder: " $cd_pre;
+#                     echo;
+#                     '
